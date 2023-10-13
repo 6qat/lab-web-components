e2278a35239465d501c872b0ae4c65d0dfa2f942
Guilherme C. B. Moreira <ceschiatti@gmail.com>
2023-10-12 23:52:18 -0300

    Before adding Mantine

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import {Button} from "@/components/ui/button";
+import {ReloadIcon} from "@radix-ui/react-icons";
 
 export default function Home() {
     return (
         <main className="">
             <h1>Home</h1>
-            <Button>Button</Button>
+            <div className="flex">
+                <Button variant="default">
+                    Button
+                </Button>
+                <Button disabled>
+                    <ReloadIcon className="mr-2 h-4 w-4 animate-spin"/>
+                    Please wait
+                </Button>
+            </div>
         </main>
     )
 }
