c9cd6384b9fd1eb859ec7ee3f969b92554ff2e70
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-10-20 13:17:58 -0300

    Using SWR to fetch data

--- app/lab/mistakes/fetch-canceling.tsx
+++ app/lab/mistakes/fetch-canceling.tsx
@@ -1,14 +1,18 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
+import useSWR, { Fetcher } from 'swr';
 
 const PostButton = () => {
     const [id, setId] = useState(1);
 
     return (
         <>
-            <Button onClick={() => setId(Math.floor(Math.random() * 100))}>Next post</Button>
-            <PostBody id={id} />
+            <Button onClick={() => setId(Math.floor(Math.random() * 100))}>
+                Next post
+            </Button>
+            {/*<PostBody id={id} />*/}
+            <PostBodyWithSWR id={id} />
         </>
     );
 };
@@ -37,5 +41,29 @@ const PostBody = ({ id }: { id: number }) => {
         </>
     );
 };
+
+// https://swr.vercel.app/docs/typescript
+function PostBodyWithSWR({ id }: { id: number }) {
+    const fetcher: Fetcher<string, string> = (url) =>
+        fetch(url)
+            .then((res) => res.json())
+            .then((json) => json.body);
+
+    const { data, error, isLoading } = useSWR(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+        fetcher
+    );
+
+    if (error) return <div>failed to load</div>;
+    if (isLoading) return <div>loading...</div>;
+
+    // render data
+    return (
+        <>
+            <p>Post id: {id}</p>
+            <div>hello {data}!</div>
+        </>
+    );
+}
 
 export default PostButton;
