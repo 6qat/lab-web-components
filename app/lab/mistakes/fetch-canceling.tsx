e4962ce5b3fa03c4039908268c40f8a053c5066b
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-10-20 07:40:25 -0300

    Complete last example

--- app/lab/mistakes/fetch-canceling.tsx
+++ app/lab/mistakes/fetch-canceling.tsx
@@ -1,4 +1,17 @@
+'use client';
 import React, { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+
+const PostButton = () => {
+    const [id, setId] = useState(1);
+
+    return (
+        <>
+            <Button onClick={() => setId(Math.floor(Math.random() * 100))}>Next post</Button>
+            <PostBody id={id} />
+        </>
+    );
+};
 
 const PostBody = ({ id }: { id: number }) => {
     const [text, setText] = useState('');
@@ -17,7 +30,12 @@ const PostBody = ({ id }: { id: number }) => {
         };
     }, [id]);
 
-    return <div>{text}</div>;
+    return (
+        <>
+            <p>Post id: {id}</p>
+            <div>{text}</div>
+        </>
+    );
 };
 
-export default PostBody;
+export default PostButton;
--- app/lab/mistakes/page.tsx
+++ app/lab/mistakes/page.tsx
@@ -8,6 +8,7 @@ import Cart from '@/app/lab/mistakes/cart';
 import Price from '@/app/lab/mistakes/price';
 import BlogPost from '@/app/lab/mistakes/blog-post';
 import CounterExample from '@/app/lab/mistakes/stale-closure';
+import PostButton from '@/app/lab/mistakes/fetch-canceling';
 
 // https://www.youtube.com/watch?v=-yIsQPp31L0
 function Page() {
@@ -29,6 +30,8 @@ function Page() {
             <BlogPost />
             <Separator className='my-6 h-2' />
             <CounterExample />
+            <Separator className='my-6 h-2' />
+            <PostButton />
             <Separator className='my-6 h-2' />
         </>
     );
