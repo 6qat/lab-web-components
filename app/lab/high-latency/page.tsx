1b95e50cbce6c94b993a6d9188f11ffab98bc931
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-11-05 18:26:25 -0300

    Create fake API to play with high latency situations

---
+++ app/lab/high-latency/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// https://www.youtube.com/watch?v=ZqerXMzt-EY
+const HighLatency = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HighLatency;
